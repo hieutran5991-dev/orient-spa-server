a517e4ee345ac89f8ad4e7a8368996b227166271
chientt <chientt@kaopiz.com>
2025-09-09 10:31:08 +0700

    update deployment

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import createMiddleware from 'next-intl/middleware';
 import { SUPPORTED_LANGUAGE, DEFAULT_LANGUAGE } from '@/utils/constants';
-import type {LocalePrefix} from "next-intl/dist/types/routing/types";
+import type { LocalePrefix } from "next-intl/dist/types/routing/types";
 
 export default createMiddleware({
   locales: SUPPORTED_LANGUAGE,
